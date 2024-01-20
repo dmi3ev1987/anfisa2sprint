b5aca6785a082a5cb3ef6330394429034c2c88b0
Андрей Дмитриев <andrey.dmi3ev@gmail.com>
2024-01-20 16:31:54 +0300

    Admin part-2.

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,11 +1,60 @@
-# ice_cream/admin.py
 from django.contrib import admin
 
-# Из модуля models импортируем модель Category...
 from .models import Category, Topping, Wrapper, IceCream
 
-# ...и регистрируем её в админке:
-admin.site.register(Category)
+admin.site.empty_value_display = 'Не задано'
+
+'''
+class IceCreamInline(admin.TabularInline):
+    model = IceCream
+    extra = 0
+'''
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    # Это свойство сработает для всех полей этой модели.
+    # Вместо пустого значения будет выводиться строка "Не задано".
+    empty_value_display = 'Не задано'
+    # Указываем, для каких связанных моделей нужно включить такой интерфейс:
+    filter_horizontal = ('toppings',)
+
+
+# Регистрируем кастомное представление админ-зоны
+# Регистрируем класс с настройками админки для моделей IceCream и Category:
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Category, CategoryAdmin)
+# Регистрируем модели Topping и Wrapper,
+# чтобы ими можно было управлять через админку
+# (интерфейс админки для этих моделей останется стандартным):
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
